1020f3925e6c9846ff2de1c3f9c0554c5a205e35
niall-brady <niallbrady98@gmail.com>
2022-10-16 15:06:56 +0100

    Fix text lingering

--- loading_bar.py
+++ loading_bar.py
@@ -1,6 +1,7 @@
 import time
 import random
 import math
+import sys
 
 
 class LoadingBar:
@@ -17,6 +18,16 @@ class LoadingBar:
 
         self.prefix_text = prefix_text
         self.suffix_text = suffix_text
+        
+        self.percent_text_prefix = " "
+        self.percent_text_suffix = "% Done"
+        
+        self.total_length = (len(self.full * self.length) +
+                             len(self.edge_left + self.edge_right) +
+                             len(self.prefix_text + self.suffix_text) +
+                             len(self.percent_text_prefix) +
+                             len(self.percent_text_suffix) +
+                             5)
 
     def show_progress(self):
         progress = self.done / self.total
@@ -25,26 +36,28 @@ class LoadingBar:
         empty_boxes = self.length - filled_boxes
 
         bar_text = self.edge_left + self.full * filled_boxes + self.empty * empty_boxes + self.edge_right
-        percent_text = f" {progress*100:.2f}% Done"
+        percent_text = self.percent_text_prefix + f"{progress*100:.2f}" + self.percent_text_suffix
 
-        msg = (self.prefix_text + bar_text + percent_text + self.suffix_text)
+        msg = self.prefix_text + bar_text + percent_text + self.suffix_text
 
         print(msg, end="") if self.done != self.total else print(msg)
+        sys.stdout.flush()
 
     def increment(self):
         self.done += 1
 
-    def print(self, msg):
-        print(f"\r{msg}")
+    def log(self, msg):
+        full_msg = msg if len(msg) >= self.total_length else msg + " " * (self.total_length - len(msg))
+        print(f"\r{full_msg}")
         self.show_progress()
 
 
 def _save_file(file, loading_bar):
-    loading_bar.print(f"Saving {file} . . .")
+    loading_bar.log(f"Saving {file} . . .")
     time.sleep(0.05 + random.random() * 1.45)
 
     loading_bar.increment()
-    loading_bar.print(f"Saved")
+    loading_bar.log("Saved")
 
 
 def _run_loop(fake_files):
@@ -60,8 +73,13 @@ def _run_loop(fake_files):
         _save_file(f"{random.choice(prefixes)}{i+1}{random.choice(extensions)}", loading_bar)
 
 
+def _pause():
+    input("\nPress Enter to continue . . . ")
+
+
 def _main():
     _run_loop(9)
+    _pause()
 
 
 if __name__ == '__main__':
